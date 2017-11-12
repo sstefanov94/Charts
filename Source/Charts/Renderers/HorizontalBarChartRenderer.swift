6b456dfb11c64b4195702eed01c2542ce818faf8
Stefan Stefanov <sstefanov94@gmail.com>
2017-11-12 19:08:45 +0100

    wip

--- Source/Charts/Renderers/HorizontalBarChartRenderer.swift
+++ Source/Charts/Renderers/HorizontalBarChartRenderer.swift
@@ -118,7 +118,7 @@ open class HorizontalBarChartRenderer: BarChartRenderer
                 barRect.origin.x = left
                 barRect.size.width = right - left
                 barRect.origin.y = top
-                barRect.size.height = bottom - top
+                barRect.size.height = bottom - top - 5
                 
                 buffer.rects[bufferIndex] = barRect
                 bufferIndex += 1
